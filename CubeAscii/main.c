2a97a989763443822847d7047a68dadf86fc97bd
Im-Rises <quentin-morel88@hotmail.com>
2023-05-22 16:17:39 -0400

    wip

--- CubeAscii/main.c
+++ CubeAscii/main.c
@@ -273,25 +273,23 @@ int main(int argc, char** argv) {
 
     if (cubeCount == 1)
     {
-        screenWidth = 60;
+        screenWidth = 70;
         screenHeight = 30;
     }
     else if (cubeCount == 2)
     {
-        /*
-         * float rotationX, float rotationY, float rotationZ, float rotationXSpeed, float rotationYSpeed,
-         * float rotationZSpeed, float cubeWidthHeight, float horizontalOffset, float verticalOffset,
-         * int distanceFromCam*/
         screenWidth = 100;
         screenHeight = 30;
-        cubeArray[1] = createCustomCube(0, 0, 0, -0.03F, 0.05F, -0.02F, 20, -30, 0, 100);
         cubeArray[0].horizontalOffset = 15;
+        cubeArray[1] = createCustomCube(0, 0, 0, -0.03F, 0.05F, -0.02F, 20, -30, 0, 100);
     }
     else if (cubeCount == 3)
     {
         screenWidth = 120;
         screenHeight = 30;
-        cubeArray[2] = createCustomCube(0, 0, 0, -0.03F, 0.05F, -0.02F, 10, +30, 0, 100);
+        cubeArray[0].horizontalOffset = 5;
+        cubeArray[1] = createCustomCube(0, 0, 0, -0.03F, 0.05F, -0.02F, 20, -45, 0, 100);
+        cubeArray[2] = createCustomCube(0, 0, 0, -0.03F, 0.05F, -0.02F, 10, +45, 0, 100);
     }
     else
     {
