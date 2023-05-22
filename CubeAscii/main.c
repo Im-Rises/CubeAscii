561435a5c131bb2c001b31558ab03c3ad5bf367e
Im-Rises <quentin-morel88@hotmail.com>
2023-05-22 13:49:59 -0400

    Reformatting all the code

--- CubeAscii/main.c
+++ CubeAscii/main.c
@@ -28,51 +28,72 @@ int screenWidth = SCREEN_WIDTH, screenHeight = SCREEN_HEIGHT;
 float zBuffer[SCREEN_SIZE];
 char buffer[SCREEN_SIZE];
 
-// struct Cube{
-//     float rotationX, rotationY, rotationZ;
-//     float rotationXSpeed, rotationYSpeed, rotationZSpeed;
-//     float cubeWidthHeight;
-// };
-
-float rotationX, rotationY, rotationZ;
-
-float rotationXSpeed = 0.05F;
-float rotationYSpeed = 0.05F;
-float rotationZSpeed = 0.01F;
-
-float cubeWidthHeight = 40;
 int distanceFromCam = 100;
-float horizontalOffset = 0;
-float verticalOffset = 0;
 float K1 = 40;
-
 float charIncrementSpeed = 1.0F;
 
-float calculateX(int i, int j, int k) {
-    return j * sin(rotationX) * sin(rotationY) * cos(rotationZ) - k * cos(rotationX) * sin(rotationY) * cos(rotationZ) +
-           j * cos(rotationX) * sin(rotationZ) + k * sin(rotationX) * sin(rotationZ) +
-           i * cos(rotationY) * cos(rotationZ);
+
+typedef struct Cube Cube;
+struct Cube {
+    float rotationX, rotationY, rotationZ;
+    float rotationXSpeed, rotationYSpeed, rotationZSpeed;
+    float cubeWidthHeight;
+    float horizontalOffset, verticalOffset;
+};
+
+Cube createCube() {
+    Cube cube;
+    cube.rotationX = 0;
+    cube.rotationY = 0;
+    cube.rotationZ = 0;
+    cube.rotationXSpeed = 0.05F;
+    cube.rotationYSpeed = 0.05F;
+    cube.rotationZSpeed = 0.01F;
+    cube.cubeWidthHeight = 40;
+    cube.horizontalOffset = 0;
+    cube.verticalOffset = 0;
+    return cube;
 }
 
-float calculateY(int i, int j, int k) {
-    return j * cos(rotationX) * cos(rotationZ) + k * sin(rotationX) * cos(rotationZ) -
-           j * sin(rotationX) * sin(rotationY) * sin(rotationZ) + k * cos(rotationX) * sin(rotationY) * sin(rotationZ) -
-           i * cos(rotationY) * sin(rotationZ);
+// Cube createCube(float rotationX, float rotationY, float rotationZ, float rotationXSpeed, float rotationYSpeed, float rotationZSpeed, float cubeWidthHeight, float horizontalOffset, float verticalOffset) {
+//     Cube cube;
+//     cube.rotationX = rotationX;
+//     cube.rotationY = rotationY;
+//     cube.rotationZ = rotationZ;
+//     cube.rotationXSpeed = rotationXSpeed;
+//     cube.rotationYSpeed = rotationYSpeed;
+//     cube.rotationZSpeed = rotationZSpeed;
+//     cube.cubeWidthHeight = cubeWidthHeight;
+//     cube.horizontalOffset = horizontalOffset;
+//     cube.verticalOffset = verticalOffset;
+//     return cube;
+// }
+
+float calculateX(int i, int j, int k, Cube* cube) {
+    return j * sin(cube->rotationX) * sin(cube->rotationY) * cos(cube->rotationZ) - k * cos(cube->rotationX) * sin(cube->rotationY) * cos(cube->rotationZ) +
+           j * cos(cube->rotationX) * sin(cube->rotationZ) + k * sin(cube->rotationX) * sin(cube->rotationZ) +
+           i * cos(cube->rotationY) * cos(cube->rotationZ);
 }
 
-float calculateZ(int i, int j, int k) {
-    return k * cos(rotationX) * cos(rotationY) - j * sin(rotationX) * cos(rotationY) + i * sin(rotationY);
+float calculateY(int i, int j, int k, Cube* cube) {
+    return j * cos(cube->rotationX) * cos(cube->rotationZ) + k * sin(cube->rotationX) * cos(cube->rotationZ) -
+           j * sin(cube->rotationX) * sin(cube->rotationY) * sin(cube->rotationZ) + k * cos(cube->rotationX) * sin(cube->rotationY) * sin(cube->rotationZ) -
+           i * cos(cube->rotationY) * sin(cube->rotationZ);
 }
 
-void calculateForSurface(float cubeX, float cubeY, float cubeZ, char ch) {
-    float x = calculateX(cubeX, cubeY, cubeZ);
-    float y = calculateY(cubeX, cubeY, cubeZ);
-    float z = calculateZ(cubeX, cubeY, cubeZ) + distanceFromCam;
+float calculateZ(int i, int j, int k, Cube* cube) {
+    return k * cos(cube->rotationX) * cos(cube->rotationY) - j * sin(cube->rotationX) * cos(cube->rotationY) + i * sin(cube->rotationY);
+}
+
+void calculateForSurface(float cubeX, float cubeY, float cubeZ, Cube* cube, char ch) {
+    float x = calculateX(cubeX, cubeY, cubeZ, cube);
+    float y = calculateY(cubeX, cubeY, cubeZ, cube);
+    float z = calculateZ(cubeX, cubeY, cubeZ, cube) + distanceFromCam;
 
     float ooz = 1 / z;
 
-    int xp = (int)(screenWidth / 2 + horizontalOffset + K1 * ooz * x * 2);
-    int yp = (int)(screenHeight / 2 + verticalOffset + K1 * ooz * y);
+    int xp = (int)(screenWidth / 2 + cube->horizontalOffset + K1 * ooz * x * 2);
+    int yp = (int)(screenHeight / 2 + cube->verticalOffset + K1 * ooz * y);
 
     int idx = xp + yp * screenWidth;
     if (idx >= 0 && idx < screenWidth * screenHeight)
@@ -85,27 +106,27 @@ void calculateForSurface(float cubeX, float cubeY, float cubeZ, char ch) {
     }
 }
 
-void updateBuffers() {
-    const int halfCubeLength = cubeWidthHeight / 2;
+void updateBuffers(Cube* cube) {
+    const int halfCubeLength = cube->cubeWidthHeight / 2;
 
     for (float cubeX = -halfCubeLength; cubeX < halfCubeLength; cubeX += charIncrementSpeed)
     {
         for (float cubeY = -halfCubeLength; cubeY < halfCubeLength; cubeY += charIncrementSpeed)
         {
-            calculateForSurface(cubeX, cubeY, -halfCubeLength, FACE_1_CHARACTER);
-            calculateForSurface(halfCubeLength, cubeY, cubeX, FACE_2_CHARACTER);
-            calculateForSurface(-halfCubeLength, cubeY, -cubeX, FACE_3_CHARACTER);
-            calculateForSurface(-cubeX, cubeY, halfCubeLength, FACE_4_CHARACTER);
-            calculateForSurface(cubeX, -halfCubeLength, -cubeY, FACE_5_CHARACTER);
-            calculateForSurface(cubeX, halfCubeLength, cubeY, FACE_6_CHARACTER);
+            calculateForSurface(cubeX, cubeY, -halfCubeLength, cube, FACE_1_CHARACTER);
+            calculateForSurface(halfCubeLength, cubeY, cubeX, cube, FACE_2_CHARACTER);
+            calculateForSurface(-halfCubeLength, cubeY, -cubeX, cube, FACE_3_CHARACTER);
+            calculateForSurface(-cubeX, cubeY, halfCubeLength, cube, FACE_4_CHARACTER);
+            calculateForSurface(cubeX, -halfCubeLength, -cubeY, cube, FACE_5_CHARACTER);
+            calculateForSurface(cubeX, halfCubeLength, cubeY, cube, FACE_6_CHARACTER);
         }
     }
 }
 
-void rotateCube() {
-    rotationX += rotationXSpeed;
-    rotationY += rotationYSpeed;
-    rotationZ += rotationZSpeed;
+void rotateCube(Cube* cube) {
+    cube->rotationX += cube->rotationXSpeed;
+    cube->rotationY += cube->rotationYSpeed;
+    cube->rotationZ += cube->rotationZSpeed;
 }
 
 void sleepMilliseconds(int milliseconds) {
@@ -157,12 +178,15 @@ void printCube() {
     }
 }
 
-int main(int argc, char** argv) {
+int main() {
     /* Initialize unicode library */
     initUnicodeLib();
 
     /* Clear screen */
     printf(ESC_CLEAR_SCREEN);
+
+    /* Initialize cube */
+    Cube cube = createCube();
 
     while (1)
     {
@@ -171,13 +195,13 @@ int main(int argc, char** argv) {
         memset(zBuffer, 0, (unsigned long)screenWidth * screenHeight * sizeof(float));
 
         /* Update buffers */
-        updateBuffers();
+        updateBuffers(&cube);
 
         /* Display buffers to console */
         printToConsole(); // printCube();
 
         /* Rotate cube */
-        rotateCube();
+        rotateCube(&cube);
 
         /* Delay */
         sleepMilliseconds(16);
